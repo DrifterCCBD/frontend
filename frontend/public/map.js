7a33a61cfc5a55798f0ff0d10076d66919a8b097
tmsavage <tobymsavage@gmail.com>
2023-04-13 14:33:49 -0400

    [FEAT] Implemented Google Map view. Centered on NYC

--- frontend/public/map.js
+++ frontend/public/map.js
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 
 import { useState, useEffect } from 'react';
 
+import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api'
+
 var mylatlng = { lat: 38.3460, lng: -0.4907 };
 var mapOptions = {
     center: mylatlng,
@@ -13,19 +15,35 @@ var mapOptions = {
     mapTypeId: google.maps.mapTypeId.ROADMAP
 };
 
-//create Map
-var map = new google.maps.Map(document.getElementById("googleMap"))
-
-// creat a directions service object to use route method
-
-
+const center = { lat: 40.7128, lng: 74.0060} // Center of NYC
 
 function MapView() {
+
+  const {} = useJsApiLoader({
+    // googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY, USE THIS INSTEAD!
+    googleMapsApiKey: "AIzaSyCZKsWmxLCQsO521oOmegTic1VhUiCONJY",
+  })
+
+  const [map, setMap] = useState(/** @type google.maps.Map */null) // Not used. For spanning back to a set location on the map.
+
+  if (!isLoaded) {
+    return <SkeltonText />
+  }
 
   return (
     <div>
     <HeaderDriver></HeaderDriver>
     <div className="container-fluid">
+      <Box position="absolute" left={0} top={0} h="50%" w ="50%">
+        {/* Google Maps Box */}
+        <GoogleMap center={center} zoom={15} mapContainerStyle={{width: '100%', height: '100%'}} options={{
+          streetViewControl: false,
+          fullscreenControl: false
+        }} onLoad={(map) => setMap(map)}>
+          <Marker position={center} /> {/* TODO: REMOVE; Places marker in the center of NYC */}
+          {/* Display markers, or directions */}
+        </GoogleMap>
+      </Box>
         <div id="googleMap">
 
         </div>
