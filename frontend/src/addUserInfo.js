ad6aec7d204e6498be9d4d29ec641c3cf40d68e0
Helena Jonsdottir <helenajonsdottir@Helenas-MacBook-Pro-3.local>
2023-05-04 12:23:17 -0400

    user info ready

--- frontend/src/addUserInfo.js
+++ frontend/src/addUserInfo.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import Header from './header';
-import { Link } from "react-router-dom";
+import { Link, json } from "react-router-dom";
 import './addUserInfo.css'
+import { Auth } from 'aws-amplify';
+import axios from 'axios';
 
 
 
@@ -9,12 +11,13 @@ const UserInfo = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
+  const [zip, setZip] = useState('');
   const [dateOfBirth, setDateOfBirth] = useState('');
-  const [driversLicense, setDriversLicense] = useState('');
-  const [carLicensePlate, setCarLicensePlate] = useState('');
-  const [carColor, setCarColor] = useState('');
-  const [carModel, setCarModel] = useState('');
-  const [ssn, setSsn] = useState('');
+  const [gender, setGender] = useState('');
+  const [user, setUser] = useState('');
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,13 +25,52 @@ const UserInfo = () => {
       firstName,
       lastName,
       address,
+      city,
+      country,
+      zip,
       dateOfBirth,
-      driversLicense,
-      carLicensePlate,
-      carColor,
-      carModel,
-      ssn,
+      gender,
+      user
     });
+
+    Auth.currentAuthenticatedUser()
+    .then( auth_user => {
+      auth_user.getSession((err, session) => {
+        if (err) {
+          throw new Error(err);
+        }
+        const username = auth_user.username
+        const sessionToken = session.getIdToken().jwtToken;
+        const submitValues = {username: username, firstName: firstName, lastName: lastName, addres: address, city: city, country: country, zip: zip, dob: dateOfBirth, gender: gender}
+        
+        axios.put('https://g6m80dg8k6.execute-api.us-east-1.amazonaws.com/prod/user/' + username, submitValues,
+        {headers: {
+          "Authorization": sessionToken
+        }}
+        )
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Failed to add user information');
+            }
+
+          })
+          .catch(error => {
+            console.log(error.message);
+          })
+
+        })
+
+      })
+
+    
+
+    if (user === 'Rider'){
+      window.location.href = '/addRiderInfo';
+    }
+    else if (user === 'Driver'){
+      window.location.href = '/addDriverInfo';
+    }
+
   };
 
   return (
@@ -50,13 +92,32 @@ const UserInfo = () => {
         <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} required/>
       </label>
       <label>
+        City:
+        <input type="text" value={city} onChange={(e) => setCity(e.target.value)} required/>
+      </label>
+      <label>
+        Country:
+        <input type="text" value={country} onChange={(e) => setCountry(e.target.value)} required/>
+      </label>
+      <label>
+        Zip:
+        <input type="text" value={zip} onChange={(e) => setZip(e.target.value)} required/>
+      </label>
+      <label>
         Date of Birth:
         <input type="date" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} required/>
       </label>
       <div className='radio-div'>
-        <input type="radio" value="Driver" name="gender" required/> Driver
-        <input type="radio" value="Rider" name="gender" required/> Rider
+        <input type="radio" value="Driver" name="user" onChange={(e) => setUser(e.target.value)} required/> Driver
+        <input type="radio" value="Rider" name="user" onChange={(e) => setUser(e.target.value)} required/> Rider
       </div>
+      <label htmlFor="gender">Gender:</label>
+              <select id="gender" name="gender" value={gender} onChange={(e) => setGender(e.target.value)} required>
+                <option value="">-- Please select --</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+                </select>
       <div>
         <Link to="/">
           <button>Back</button>
