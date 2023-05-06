e9a4cee595a56cb6977a35dc4d39bf7ff14b9980
Ben Allison <bja2142@columbia.edu>
2023-05-06 18:32:35 +0000

    it's working

--- frontend/src/availableRoutesRider.js
+++ frontend/src/availableRoutesRider.js
@@ -3,14 +3,23 @@ import './headerRider.css'
 import './index.css'
 import './mytripsRider.css'
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 import { Auth } from 'aws-amplify';
 import { useState, useEffect } from 'react';
+import axios from "axios";
+
+
+
+
 
 function AvailableRoutesRider() {
 
   const [data, setData] = useState([]);
   const [available_trips, setAvailableTrips] = useState([]);
+  const [userToken, setUserToken] = useState("");
+
+
+
 
   useEffect(() => {
     Auth.currentAuthenticatedUser()
@@ -22,9 +31,10 @@ function AvailableRoutesRider() {
         }
         console.log(session);
         const sessionToken = session.getIdToken().jwtToken;
+        setUserToken(sessionToken);
       
     // Make the fetch request for all available trips
-    fetch('https://g6m80dg8k6.execute-api.us-east-1.amazonaws.com/prod/trip?available=true', {
+    fetch('https://g6m80dg8k6.execute-api.us-east-1.amazonaws.com/prod/trip?available=true&username=' + user.username, {
       headers: {
         Authorization: sessionToken
       }
@@ -48,6 +58,37 @@ function AvailableRoutesRider() {
 
     }, []);
 
+    const navigate = useNavigate();
+
+
+    const requestToJoinTrip = (trip_id) => {
+      const updateTrip = axios.put(
+        "https://g6m80dg8k6.execute-api.us-east-1.amazonaws.com/prod/trip/" + trip_id, {},
+        {
+          headers: {
+            Authorization: userToken,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res)
+        if (res.data.statusCode == 500) {
+          throw new Error(res.data.body);
+        } else {
+          navigate('/mytripsRider', { replace: true });
+        }
+        
+        })
+      .catch((error) => {
+        alert(
+          "Error saving user information:" + error
+        );
+      })
+      .finally(() => {
+
+      });
+    }
+
 
   return (
     <div>
@@ -56,6 +97,7 @@ function AvailableRoutesRider() {
         <h2>All available trips</h2>
         <table className="mytrips-table">
           <thead>
+            <th>Trip ID</th>
             <th>Start Date and Time</th>
             <th>Destination</th>
             <th>Origin</th>
@@ -65,14 +107,15 @@ function AvailableRoutesRider() {
           <tbody>
             {available_trips.map(item => (
               <tr key={item.id}>
-                {Object.keys(item).map(key => (
+                {Object.keys(item).map(key => 
+                  (
                   <td key={key}>{item[key]}</td>
-                ))}
-                <td>
-                  Select 
-                  {/* <button onClick={() => window.location.href='/maps'}>
+                  )
+                )}
+                <td> 
+                  {<button onClick={() => requestToJoinTrip(item.trip_id)}>
                     Select
-                  </button> */}
+                  </button> }
                 </td>
               </tr>
             ))}
