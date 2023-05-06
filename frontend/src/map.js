773da27539330e39d9dd74bcf0845f6407aafe6f
tmsavage <tobymsavage@gmail.com>
2023-05-06 10:25:36 -0400

    updated code for map [still looping]

--- frontend/src/map.js
+++ frontend/src/map.js
@@ -12,40 +12,42 @@ import {
     Text,
 } from '@chakra-ui/react'
 import { Link } from "react-router-dom";
-
+import queryString from 'query-string';
 import { useState, useEffect } from 'react';
+import { debounce } from 'lodash';
 // import { FaLocationArrow, FaTimes } from 'react-icons/fa'
 
 import { useJsApiLoader, GoogleMap, DirectionsRenderer } from '@react-google-maps/api'
 // import { Autocomplete } from "@aws-amplify/ui-react";
 
 const center = { lat: 40.7129, lng: -74.0072 } // NYC
+const libraries = ['places'];
 // const center = { lat: 48.8584, lng: 2.2945 } // NYC
 
-const deploy_with_api_key = false;
+const deploy_with_api_key = true;
 
 function Map() { 
 
     const { isLoaded } = useJsApiLoader({
-        googleMapsApiKey: (deploy_with_api_key) ? process.env.REACT_APP_GOOGLE_MAPS_API_KEY: "undefined",
-        libraries: ['places'],
+        // googleMapsApiKey: (deploy_with_api_key) ? process.env.REACT_APP_GOOGLE_MAPS_API_KEY: "AIzaSyCZKsWmxLCQsO521oOmegTic1VhUiCONJY",
+        googleMapsApiKey: "AIzaSyCZKsWmxLCQsO521oOmegTic1VhUiCONJY",
+        libraries: libraries,
     })
 
+    const values = queryString.parse(window.location.search);
+    // const origin = 
+    // const destination = 
     const [map, setMap] = useState(/** @type google.maps.Map */ null)
     const [directionsResponse, setDirectionsResponse] = useState(null)
     const [distance, setDistance] = useState('')
     const [duration, setDuration] = useState('')
-    const origin_input = "Hell's Kitchen, NY"
-    const destination_input = "Lenox Hill, NY"
+    let origin_input = values.origin
+    let destination_input = values.destination
     const driver_name = "Toby Savage"
     const driver_car = "Jeep Wrangler"
     const trip_depart_time = "12:14 PM"
     const trip_arrive_time = "12:41 PM"
     const [buttonPopup, setButtonPopup] = useState(false);
-
-    // const originRef = useRef()
-    // const destinationRef = useRef()
-
 
     if (!isLoaded) {
         return <div>
@@ -56,36 +58,31 @@ function Map() {
         {/* <button className="logout-button">Log out</button> */}
         </div>
     }
-
+    // const originRef = useRef()
+    // const destinationRef = useRef()
+    // useEffect(() => {
+    console.log("HERE!", origin_input)
     async function calculateRoute() {
         if (origin_input === '' || destination_input === '') {
+            
             return 
         }
-        const directionsService = new google.maps.DirectionsService()
+        console.log("HEH?")
+        let directionsService = new google.maps.DirectionsService()
         const results = await directionsService.route({
             origin: origin_input,
             destination: destination_input,
             travelMode: google.maps.TravelMode.DRIVING
         })
         setDirectionsResponse(results)
-        setDistance(results.routs[0].legs[0].distance.text)
-        setDuration(results.routs[0].legs[0].duration.text)
+        setDistance(results.routes[0].legs[0].distance.text)
+        setDuration(results.routes[0].legs[0].duration.text)
+        directionsService = null; 
     }
-    
-  
-    function clearRoute() {
-        setDirectionsResponse(null)
-        setDistance('')
-        setDuration('')
-        // originRef.current.value=''
-        // destinationRef.current.value=''
-    }
-
-    // useEffect(() => {
-    //     calculateRoute();
-    //   }, [directionsResponse, map]);
+    console.log("HERE 2!")
     calculateRoute();
-    
+
+    //   }, [values.origin, values.destination]);
 
     return (
         <div>
